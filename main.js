21b0ea9ca17151a227c575399dec8a1b0618de81
NaserHamitou <nas.hmt44@gmail.com>
2024-03-04 00:02:17 -0500

    .

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
-import './style.css'
-import { clickButton } from './counter.js'
-
 document.querySelector('#app').innerHTML = `
   <div id="container">
     <h1>Griddy is not availabe 😔😔😔</h1>
@@ -8,5 +5,34 @@ document.querySelector('#app').innerHTML = `
     </div>
   </div>
 `
+export async function clickButton(element) {
+
+  let obj = '';
+  const cliclEvent = () => {
+    console.log('click')
+    callAPI().then((response) => {
+      response.json().then((data) => {
+        console.log(JSON.stringify(data))
+        obj = JSON.stringify(data)
+        if (obj.includes('Griddy') || obj.includes('griddy')) {
+          element.style.backgroundColor = 'green';
+          element.style.color = 'white';
+          element.querySelector('h1').innerHTML = 'Griddy PRESENT 🥵💯🥵💯🥵💯🥵💯🥵💯';
+          new Notification('Griddy PRESENT 🥵💯🥵💯🥵💯🥵💯🥵💯');
+        }
+      })
+    })
+    setTimeout(cliclEvent, 5000);
+  }
+
+  await Notification.requestPermission();
+  cliclEvent()
+
+}
+
+
+export async function callAPI() {
+  return await fetch('https://fortnite-api.com/v2/shop/br');
+}
 
 clickButton(document.querySelector('#container'))
